c27e3d84a01c0d1692b7dbd37a2cd9c858769204
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 14:56:52 +0530

    Update and rename README.md to integer.java

---
+++ integer.java
@@ -0,0 +1,24 @@
+# guvi9
+import java.util.Scanner;
+class Ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+   int Number, Count=0;
+   Scanner sc=new Scanner(System.in);
+   sc = new Scanner(System.in);		
+   Number = sc.nextInt();
+   while(Number > 0) 
+   {
+	Number = Number / 10;
+	Count = Count + 1; 
+    }
+    System.out.println(Count);
+ }
+}
+
+
+        
+      
+       
+  
